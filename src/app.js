1a7756ba7ccee34bc086f85becf4bd6138ed17a9
Nikita <sabaoth@ukr.net>
2020-03-05 07:25:40 +0200

    all css done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import './scss/base.scss'
+import * as elements from './js/base';
+
+
+
+// Global App Controller 
+
+const controller = (() => {
+    document.addEventListener('click', ctrlDeleteItem);
+
+    function ctrlDeleteItem(event) {
+        console.log(event.target.parentNode.parentNode.parentNode.parentNode.id);
+    }
+})();
---
+++ src/js/base.js
@@ -0,0 +1,4 @@
+export const DOMstrings = {
+    products: document.querySelector('.products'),
+    arrows: document.querySelector('.slider__arrows')
+}
